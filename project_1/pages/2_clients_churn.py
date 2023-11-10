9e9a3bf2f7accfd10c3cf5ab3510c8755b263933
Sazuppy <sazuppy@gmail.com>
2023-11-10 22:39:19 +0300

    Update 2_clients_churn.py

--- project_1/pages/2_clients_churn.py
+++ project_1/pages/2_clients_churn.py
@@ -1,96 +1,119 @@
 import streamlit as st
+import numpy as np
 import pandas as pd
-import pydeck as pdk
-from urllib.error import URLError
+import pickle 
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 
-st.set_page_config(page_title="Mapping Demo", page_icon="🌍")
+st.set_page_config(page_title="# Отток клиентов", page_icon="📈")
 
-st.markdown("# Mapping Demo")
-st.sidebar.header("Mapping Demo")
+st.markdown('# Отток клиентов')
+
 st.write(
-    """This demo shows how to use
-[`st.pydeck_chart`](https://docs.streamlit.io/library/api-reference/charts/st.pydeck_chart)
-to display geospatial data."""
+    """Из «Бета-Банка» стали уходить клиенты. Каждый месяц. Немного, но заметно. Банковские маркетологи посчитали: сохранять текущих клиентов дешевле, чем привлекать новых.
+
+Нужно спрогнозировать, уйдёт клиент из банка в ближайшее время или нет. Вам предоставлены исторические данные о поведении клиентов и расторжении договоров с банком. 
+
+Постройте модель с предельно большим значением *F1*-меры. Чтобы сдать проект успешно, нужно довести метрику до 0.59. Проверьте *F1*-меру на тестовой выборке самостоятельно.
+
+Дополнительно измеряйте *AUC-ROC*, сравнивайте её значение с *F1*-мерой.
+
+Источник данных: [https://www.kaggle.com/barelydedicated/bank-customer-churn-modeling](https://www.kaggle.com/barelydedicated/bank-customer-churn-modeling)
+    
+    Описание данных на которых модель была обучена:     
+
+    Признаки:
+- CreditScore — кредитный рейтинг
+- Geography — страна проживания
+- Gender — пол
+- Age — возраст
+- Tenure — сколько лет человек является клиентом банка
+- Balance — баланс на счёте
+- NumOfProducts — количество продуктов банка, используемых клиентом
+- HasCrCard — наличие кредитной карты
+- IsActiveMember — активность клиента
+- EstimatedSalary — предполагаемая зарплата
+
+Целевой признак:  
+- Exited — факт ухода клиента
+    """
 )
 
+st.sidebar.header("Признаки для модели машинного обучения")
 
-@st.cache_data
-def from_data_file(filename):
-    url = (
-        "http://raw.githubusercontent.com/streamlit/"
-        "example-data/master/hello/v1/%s" % filename
-    )
-    return pd.read_json(url)
+def user_input_features():
+    credit_score = st.sidebar.slider('Кредитный рейтинг', 350, 850, 500)
+    geography = st.sidebar.selectbox('Страна проживания', ('France', 'Spain', 'Germany'))
+    gender = st.sidebar.selectbox('Пол', ('Female', 'Male'))
+    age = st.sidebar.slider('Возраст', 18, 92, 25)
+    tenure = st.sidebar.slider('Сколько лет человек является клиентом банка', 0, 10, 7)
+    balance = st.sidebar.slider('Баланс на счёте', 0, 300000, 20000)
+    num_of_products = st.sidebar.slider('Количество продуктов банка, используемых клиентом', 1, 4, 1)
+    has_cr_card = st.sidebar.selectbox('Наличие кредитной карты', ('Yes', 'No'))
+    is_active_member = st.sidebar.selectbox('Активность клиента', ('Yes', 'No'))
+    estimated_salary = st.sidebar.slider('Предполагаемая зарплата', 0, 200000, 10000)
+    
+    
+    data = {'credit_score': credit_score,
+            'geography': geography,
+            'gender': gender,
+            'age': age,
+            'tenure': tenure,
+            'balance': balance,
+            'num_of_products': num_of_products,
+            'has_cr_card': has_cr_card,
+            'is_active_member': is_active_member,
+            'estimated_salary': estimated_salary}
+    features = pd.DataFrame(data, index=[0])
+    return features
 
+df = user_input_features()
 
-try:
-    ALL_LAYERS = {
-        "Bike Rentals": pdk.Layer(
-            "HexagonLayer",
-            data=from_data_file("bike_rental_stats.json"),
-            get_position=["lon", "lat"],
-            radius=200,
-            elevation_scale=4,
-            elevation_range=[0, 1000],
-            extruded=True,
-        ),
-        "Bart Stop Exits": pdk.Layer(
-            "ScatterplotLayer",
-            data=from_data_file("bart_stop_stats.json"),
-            get_position=["lon", "lat"],
-            get_color=[200, 30, 0, 160],
-            get_radius="[exits]",
-            radius_scale=0.05,
-        ),
-        "Bart Stop Names": pdk.Layer(
-            "TextLayer",
-            data=from_data_file("bart_stop_stats.json"),
-            get_position=["lon", "lat"],
-            get_text="name",
-            get_color=[0, 0, 0, 200],
-            get_size=15,
-            get_alignment_baseline="'bottom'",
-        ),
-        "Outbound Flow": pdk.Layer(
-            "ArcLayer",
-            data=from_data_file("bart_path_stats.json"),
-            get_source_position=["lon", "lat"],
-            get_target_position=["lon2", "lat2"],
-            get_source_color=[200, 30, 0, 160],
-            get_target_color=[200, 30, 0, 160],
-            auto_highlight=True,
-            width_scale=0.0001,
-            get_width="outbound",
-            width_min_pixels=3,
-            width_max_pixels=30,
-        ),
-    }
-    st.sidebar.markdown("### Map Layers")
-    selected_layers = [
-        layer
-        for layer_name, layer in ALL_LAYERS.items()
-        if st.sidebar.checkbox(layer_name, True)
-    ]
-    if selected_layers:
-        st.pydeck_chart(
-            pdk.Deck(
-                map_style="mapbox://styles/mapbox/light-v9",
-                initial_view_state={
-                    "latitude": 37.76,
-                    "longitude": -122.4,
-                    "zoom": 11,
-                    "pitch": 50,
-                },
-                layers=selected_layers,
-            )
-        )
+st.subheader('Таблица с введенными вами параметрами:')
+st.write(df)
+
+def pre_category(data):
+    if data == "Yes":
+        return 1
     else:
-        st.error("Please choose at least one layer above.")
-except URLError as e:
-    st.error(
-        """
-        **This demo requires internet access.**
-        Connection error: %s
-    """
-        % e.reason
-    )
+        return 0
+
+def preprocessing_data(df, scaler, ohe):
+    df['has_cr_card'] = df['has_cr_card'].apply(pre_category)
+    df['is_active_member'] = df['is_active_member'].apply(pre_category)
+    numeric = ['credit_score', 'age', 'tenure', 'balance', 'num_of_products', 'estimated_salary']
+    categorical = ['geography', 'gender']
+    # st.write(df)
+    df[numeric] = scaler.transform(df[numeric])
+    # st.write(df)
+    tmp = pd.DataFrame(ohe.transform(df[categorical]).toarray(), 
+                                   columns=ohe.get_feature_names_out(),
+                                   index=df.index)
+    df.drop(categorical, axis=1, inplace=True)
+    df = df.join(tmp)
+    st.write(df)
+            
+    return pd.DataFrame(df, index=[0])
+    
+@st.cache_resource
+def get_model():
+    load_model = pickle.load(open('models/clients_churn.pkl', 'rb'))
+    ohe_model = pickle.load(open('models/ohe_clients_churn.pkl', 'rb'))
+    scaler_model = pickle.load(open('models/scaler_clients_churn.pkl', 'rb'))
+    return load_model, scaler_model, ohe_model
+
+model, sc_model, ohe_model = get_model()
+
+df_new = preprocessing_data(df, sc_model, ohe_model)
+# st.write(df)
+
+prediction = model.predict(df_new)
+prediction_proba = model.predict_proba(df_new)
+
+
+st.subheader('Рекомендация')
+exited = np.array(['Клиент вероятно уйдет','Клиент вероятно останется'])
+st.write(exited[prediction])
+
+st.subheader('Вероятность рекомендации')
+st.write(prediction_proba)

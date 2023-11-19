7087162ef96042e7964daaa6513bc4d104aed972
Sazuppy <sazuppy@gmail.com>
2023-11-19 16:12:33 +0300

    Update 10_taxi_orders_prediction.py

--- project_1/pages/10_taxi_orders_prediction.py
+++ project_1/pages/10_taxi_orders_prediction.py
@@ -1,96 +1,84 @@
 import streamlit as st
+import numpy as np
 import pandas as pd
-import pydeck as pdk
-from urllib.error import URLError
+import pickle 
+from catboost import CatBoostRegressor
+import datetime
 
-st.set_page_config(page_title="Mapping Demo", page_icon="🌍")
+st.set_page_config(page_title="# Прогнозирование заказов такси", page_icon="📈")
 
-st.markdown("# Mapping Demo")
-st.sidebar.header("Mapping Demo")
-st.write(
-    """This demo shows how to use
-[`st.pydeck_chart`](https://docs.streamlit.io/library/api-reference/charts/st.pydeck_chart)
-to display geospatial data."""
-)
+st.markdown('# Прогнозирование заказов такси')
 
+with st.expander("Описание проекта"):
+    st.write("""
+        Компания «Чётенькое такси» собрала исторические данные о заказах такси в аэропортах. 
+        Чтобы привлекать больше водителей в период пиковой нагрузки, нужно спрогнозировать количество заказов такси на следующий час.
+        
+        Описание данных:
+        - datetime - время заказа
+        - num_orders - число заказов
+    """)
 
-@st.cache_data
-def from_data_file(filename):
-    url = (
-        "http://raw.githubusercontent.com/streamlit/"
-        "example-data/master/hello/v1/%s" % filename
-    )
-    return pd.read_json(url)
+df_old = pd.read_csv('D:/GitHub/yandex_project/18_taxi_orders_prediction/taxi.csv', index_col=[0], parse_dates=[0]).sort_index().resample('1H').sum()
 
 
-try:
-    ALL_LAYERS = {
-        "Bike Rentals": pdk.Layer(
-            "HexagonLayer",
-            data=from_data_file("bike_rental_stats.json"),
-            get_position=["lon", "lat"],
-            radius=200,
-            elevation_scale=4,
-            elevation_range=[0, 1000],
-            extruded=True,
-        ),
-        "Bart Stop Exits": pdk.Layer(
-            "ScatterplotLayer",
-            data=from_data_file("bart_stop_stats.json"),
-            get_position=["lon", "lat"],
-            get_color=[200, 30, 0, 160],
-            get_radius="[exits]",
-            radius_scale=0.05,
-        ),
-        "Bart Stop Names": pdk.Layer(
-            "TextLayer",
-            data=from_data_file("bart_stop_stats.json"),
-            get_position=["lon", "lat"],
-            get_text="name",
-            get_color=[0, 0, 0, 200],
-            get_size=15,
-            get_alignment_baseline="'bottom'",
-        ),
-        "Outbound Flow": pdk.Layer(
-            "ArcLayer",
-            data=from_data_file("bart_path_stats.json"),
-            get_source_position=["lon", "lat"],
-            get_target_position=["lon2", "lat2"],
-            get_source_color=[200, 30, 0, 160],
-            get_target_color=[200, 30, 0, 160],
-            auto_highlight=True,
-            width_scale=0.0001,
-            get_width="outbound",
-            width_min_pixels=3,
-            width_max_pixels=30,
-        ),
-    }
-    st.sidebar.markdown("### Map Layers")
-    selected_layers = [
-        layer
-        for layer_name, layer in ALL_LAYERS.items()
-        if st.sidebar.checkbox(layer_name, True)
-    ]
-    if selected_layers:
-        st.pydeck_chart(
-            pdk.Deck(
-                map_style="mapbox://styles/mapbox/light-v9",
-                initial_view_state={
-                    "latitude": 37.76,
-                    "longitude": -122.4,
-                    "zoom": 11,
-                    "pitch": 50,
-                },
-                layers=selected_layers,
-            )
-        )
-    else:
-        st.error("Please choose at least one layer above.")
-except URLError as e:
-    st.error(
-        """
-        **This demo requires internet access.**
-        Connection error: %s
-    """
-        % e.reason
-    )
+def user_input_features():
+    date = st.date_input("дата заказа такси", datetime.date(2018, 9, 6))
+    time = st.time_input('время заказа такси', datetime.time(8, 45))
+    target_datetime  = datetime.datetime.combine(date, time)
+    data = {'datetime': target_datetime }
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+df = user_input_features()
+df = df.sort_index(axis=1)
+
+
+   
+def preprocessing_data(data, max_lag, rolling_mean_size, target_datetime):
+    df_new = data.copy()
+    df_new['month'] = df_new.index.month
+    df_new['day'] = df_new.index.day
+    df_new['dayofweek'] = df_new.index.dayofweek
+    df_new['hour'] = df_new.index.hour
+    # Создаем признаки - значения за предыдущие периоды
+    for lag in range(1, max_lag + 1):
+        df_new[f'lag_{lag}'] = df_new['num_orders'].shift(lag)
+    # Создаем признак "скользящее среднее"
+    df_new['rolling_mean'] = df_new['num_orders'].shift().rolling(rolling_mean_size).mean()
+    # Удаляем пропуски
+    df_new = df_new.dropna(axis=0)
+    
+    # Создаем DataFrame для target_datetime
+    target_df = target_datetime.copy()
+    target_df['month'] = target_df['datetime'].dt.month
+    target_df['day'] = target_df['datetime'].dt.day
+    target_df['dayofweek'] = target_df['datetime'].dt.dayofweek
+    target_df['hour'] = target_df['datetime'].dt.hour
+    target_df = target_df.set_index('datetime')
+    
+    # Создаем признаки - значения за предыдущие периоды для target_datetime
+    for lag in range(1, max_lag + 1):
+        target_df[f'lag_{lag}'] = df_new['num_orders'].shift(lag).iloc[-1]
+    
+    # Создаем признак "скользящее среднее" для target_datetime
+    target_df['rolling_mean'] = df_new['num_orders'].shift().rolling(rolling_mean_size).mean().iloc[-1]
+
+    return target_df
+    
+            
+
+    
+@st.cache_resource
+def get_model():
+    load_model = pickle.load(open('project_1/models/taxi_orders_prediction.pkl', 'rb'))
+    
+    return load_model
+
+model = get_model()
+target_datetime = pd.to_datetime(df['datetime'].iloc[0])
+features_for_prediction = preprocessing_data(df_old, 10, 10, df)
+
+prediction = model.predict(features_for_prediction)
+st.subheader('Прогназируемое количество заказов:')
+st.write(str(round(prediction[0])))
